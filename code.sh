ee192c1937fc005949a92ba059b2c3a5153a77c6
deepnoobdev <80971758+deepnoobdev@users.noreply.github.com>
2021-03-19 22:55:33 +0530

    Create code.sh

---
+++ code.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+red=`tput setaf 1`
+read -p "Enter the key : " key
+echo "${red}
+ =================================================
+|                                                 |
+|                     My-Ceds                     |           
+|                                                 |
+ ================== deepnoobdev ==================
+${reset}"
+echo " "
+#check the website is alive
+#pastebin/$key
